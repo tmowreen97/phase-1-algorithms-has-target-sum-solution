f1d5313070875993597d87dc3549b8701753aa73
Tasreen Mowreen <tmowreen97@gmail.com>
2022-04-14 00:26:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // create an object to keep track of all the numbers we've seen
+  const seenNumbers = {}
+  //iterate over the array of numbers
+  for (const number of array){
+    //   for the current number, identify a complementary number thats adds to our target
+    const complement = target - number;
+    //   check if any of the keys in our object is the complement to the current number
+    //     if so, return true
+    if (seenNumbers[complement]) return true;
+    //   save the current number as the key on our object so we can check it later against other numbers
+    seenNumbers[number]=true;
+  }
+  // if we reach the end of the array, return false.
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Time Complexity O(n)
+  Space Complexity O(n)
 */
 
 /* 
   Add your pseudocode here
+  Option 1
+  iterate over the array of numbers
+    for the current nuber, identify a complementary number that adds to our target
+    (ex. if our number is 2, and the target is 5, the complementary is 5-2 = 3)
+    iterate over the remaining numbers in the array
+      check if any of the remaining numbers is the complement
+        if so, return true
+  if we reach the end of the array, return false.
+
+  Option 2
+  create an object to keep track of all the numbers we've seen.
+  iterate over the array of numbers.
+    for the current number, identify a complementary number thats adds to our target
+    check if any of the keys in our object is the complement to the current number
+      if so, return true
+    save the current number as the key on our object so we can check it later against other numbers
+  later against other numbers
+  if we reach the end of the array, return false.
 */
 
 /*
   Add written explanation of your solution here
+  Since iterating through an object takes less time, trying that approach here instead.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +63,14 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([4, 0], 4));
+
+
 }
 
 module.exports = hasTargetSum;
